9fd8876e8b523d55d3549479fee0c43c7aa5f206
mr_majed7 <majedulislam7789@gmail.com>
2025-05-27 23:21:17 +0600

    Initialize ride-buddies project with Maven setup, including POM file, wrapper scripts, and basic application structure.

---
+++ backend/ride-buddies/src/test/java/com/ridebuddies/ride_buddies/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridebuddies.ride_buddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
